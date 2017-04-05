27af73bce5e4724d4bc6bb4d20ecd0d0208eaf71
sharmashivam225 <sharmashivam225@gmail.com>
2017-04-05 14:26:10 +0530

    Quick fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-  validates :first_name,:last_name,:contact_no,:address,:age,:sex,:nationality,:countray,:city,:zip, presence: true
+  validates :first_name,:last_name,:contact_no,:address,:age,:sex,:nationality,:country,:city,:zip, presence: true
   validates :age, numericality: true
 
   def full_name
     "#{self.first_name} #{self.last_name}"
   end
+
 
 end
